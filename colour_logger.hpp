aa39dab2491a18f43774d4d094b3832828d8dbbf
David Jedeikin <d.jedeikin@funkflow.com>
2024-10-16 22:19:19 +0100

    Minor

--- colour_logger.hpp
+++ colour_logger.hpp
@@ -2,37 +2,28 @@
 
 #include <cstdlib>
 #include <iostream>
+#include <map>
 #include <memory>
 #include <string>
 
 class ColourLogger {
 private:
-  enum class Colour { Red, Green, Yellow, None };
-
   const std::string preface_;
-  const std::string RED_COLOUR = "\033[31m";
-  const std::string GREEN_COLOUR = "\033[32m";
-  const std::string YELLOW_COLOUR = "\033[33m";
-  const std::string RESET = "\033[0m";
 
-  std::string colour(Colour colour) {
-    switch (colour) {
-    case Colour::Green:
-      return GREEN_COLOUR;
-    case Colour::Yellow:
-      return YELLOW_COLOUR;
-    case Colour::Red:
-      return RED_COLOUR;
-    default:
-      return RESET;
-    }
+  enum class Colour { Red, Green, Yellow, None };
+  const std::map<Colour, std::string> colours_{ std::pair(Colour::None, "\033[0m"),
+                                                std::pair(Colour::Green, "\033[32m"),
+                                                std::pair(Colour::Red, "\033[31m"),
+                                                std::pair(Colour::Yellow, "\033[33m") };
+
+  auto log(std::string&& msg, Colour colour) -> void {
+    auto full_msg =
+        this->colours_.at(colour) + this->preface_ + msg + this->colours_.at(Colour::None);
+    std::cout << full_msg << '\n';
   }
 
-  auto log(std::string &&msg, Colour colour) -> void {
-    std::cout << this->colour(colour) + this->preface_ + msg + RESET << '\n';
-  }
-
-  template <typename T> static constexpr auto decayToCString(T &&arg) {
+  template <typename T>
+  static constexpr auto decayToCString(T&& arg) {
     if constexpr (std::is_same_v<std::decay_t<T>, std::string>) {
       return arg.c_str();
     } else {
@@ -41,17 +32,24 @@ private:
   }
 
 public:
-  ColourLogger(std::string_view preface) : preface_(preface) {}
+  explicit ColourLogger(std::string_view preface) : preface_(preface) {
+  }
 
-  void logRaw(std::string &&msg) { this->log(std::move(msg), Colour::None); }
-  void logGreen(std::string &&msg) { this->log(std::move(msg), Colour::Green); }
-  void logRed(std::string &&msg) { this->log(std::move(msg), Colour::Red); }
-  void logYellow(std::string &&msg) {
+  void logRaw(std::string&& msg) {
+    this->log(std::move(msg), Colour::None);
+  }
+  void logGreen(std::string&& msg) {
+    this->log(std::move(msg), Colour::Green);
+  }
+  void logRed(std::string&& msg) {
+    this->log(std::move(msg), Colour::Red);
+  }
+  void logYellow(std::string&& msg) {
     this->log(std::move(msg), Colour::Yellow);
   }
 
   template <typename... Args>
-  static std::string format(const char *format, Args... args) {
+  static auto format(const char* format, Args... args) -> std::string {
     // Calculate the number of character that would be written, plus one for
     // null termination
     int size_s = snprintf(nullptr, 0, format, decayToCString(args)...) + 1;
@@ -61,6 +59,6 @@ public:
     // Write the formatted string to the allocated buffer
     snprintf(buf.get(), size, format, decayToCString(args)...);
     // Create a string using the a pointer to the bufer
-    return {buf.get()};
+    return { buf.get() };
   }
 };
